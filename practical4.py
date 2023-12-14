8977758af244336f24fea9d5b0c97f8cd4dd5a14
0xyshv <writetoyshv@gmail.com>
2023-12-14 22:29:10 +0530

    fix: simpler code for factor

--- practical4.py
+++ practical4.py
@@ -1,16 +1,13 @@
-def find_factors(n):
-    factors = []
+def print_factors(n):
+    print(f"Factors of {n} are:")
     for i in range(1, n + 1):
         if n % i == 0:
-            factors.append(i)
-    return factors
+            print(i)
 
-# Get input from the user
-try:
-    num = int(input("Enter a number: "))
-    factors_of_num = find_factors(num)
-    print(f"Factors of {num} are: {', '.join(map(str, factors_of_num))}")
+# Get user input
+user_input = int(input("Enter a number: "))
 
-except ValueError:
-    print("Please enter a valid integer.")
+# Call the function to print factors
+print_factors(user_input)
+
 
